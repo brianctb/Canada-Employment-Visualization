19d840df48ddc9e85ecc174ebf3c38bfd037edfa
Brian Lee <clee689@my.bcit.ca>
2023-06-12 20:15:33 -0700

    create database connection

---
+++ Backend/database.js
@@ -0,0 +1,13 @@
+const db = require('mongoose');
+
+db.connect('mongodb://localhost:27017/express', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to database');
+}).catch((err) => {
+    console.log('Unable to connect to database');
+    console.log(err);
+});
+
+exports.db = db;
