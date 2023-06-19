41bead55658f6cb4fdc80eb5b162dfc6e676b0dd
Brian Lee <clee689@my.bcit.ca>
2023-06-19 01:09:53 -0700

    refactor code, implement dropdown menu with select

--- Frontend/src/viewyear.js
+++ Frontend/src/viewyear.js
@@ -3,27 +3,44 @@ import { useLocation } from "react-router-dom";
 import CenterDiv from "./components/center_div";
 import ChartDiv from "./components/chart_div";
 import Barchart from "./components/Barchart";
-import Dropdown from "./components/dropdown";
-import { Chart } from "chart.js";
+import Select from "react-select";
 
 function Viewyear({ data }) {
     const location = useLocation();
     const { detail } = location.state;
     const result = data.find((item) => item.Sort_type === detail);
-    const options = [
-        { value: "All Industries", label: "All Industries" },
-        { value: "Goods Producing", label: "Goods Producing" },
-        { value: "Service Producing", label: "Service Producing" },
-        // "All Industries", "Goods Producing", "Service Producing"
-    ];
+
+    const options = Object.keys(result).filter(
+        names => (names !== "Sort_type" && names !== "_id")
+    ).map(name => ({
+        value: name, label: name.replace("_", " ")
+    }));
+
+    
+    const [selectedOption, setSelectedOption] = useState(options[0].value);
+    const handleDropdownChange = (selectedOption) => {
+        setSelectedOption(selectedOption.value);
+    };
+
+    const styles = {
+        option: (styles, { data }) => ({
+            ...styles,
+            color: '#000000',
+            backgroundColor: data.value === selectedOption ? "grey" : 'transparent'
+        }),
+        placeholder: (styles) => ({
+            ...styles,
+            color: '#000000',
+        }),
+    }
 
     return (
         <ChartDiv>
             <CenterDiv>
-                <h2>Currently showcasing</h2>
-                <Dropdown options={options} />
+                <h3>Pleaes select the dataset:</h3>
+                <Select placeholder={selectedOption} value={selectedOption} options={options} styles={styles} onChange={handleDropdownChange} />
             </CenterDiv>
-            <Barchart object={result} detail={detail} />
+            <Barchart object={result} detail={detail} selectedOption={selectedOption} />
             <CenterDiv>
 
             </CenterDiv>
