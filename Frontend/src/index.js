83e440abddc4474c49ae8d3d36fada9029eecc05
brianctb <brianlee1188@gmail.com>
2025-06-09 14:48:28 -0700

    remove hardcode api url

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './css/index.css';
-import Homepage from './homepage';
-import Years from './years';
-import Industries from './industry';
-import View from './view';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./css/index.css";
+import Homepage from "./homepage";
+import Years from "./years";
+import Industries from "./industry";
+import View from "./view";
+import reportWebVitals from "./reportWebVitals";
+import { API_BASE_URL } from "./config";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const fetchYear = async () => {
-  const response = await fetch('https://puce-giant-rhinoceros.cyclic.app/api/by_year');
+  const response = await fetch(`${API_BASE_URL}/api/by_year`);
   const yeardata = await response.json();
   return yeardata;
 };
 
 const fetchIndustries = async () => {
-  const response = await fetch('https://puce-giant-rhinoceros.cyclic.app/api/by_industry');
+  const response = await fetch(`${API_BASE_URL}/api/by_industry`);
   const industrydata = await response.json();
   return industrydata;
 };
@@ -28,27 +29,34 @@ var industrydata = [];
 const fetchdata = async () => {
   yeardata = await fetchYear();
   industrydata = await fetchIndustries();
-}
+};
 
-fetchdata()
-  .then(() => {
-    root.render(
-      <React.StrictMode>
-        <Router>
-          <Routes>
-            <Route path='/' element={<Homepage />} />
-            <Route path='/years' element={<Years data={yeardata} />} />
-            <Route path='/viewyear' element={<View data={yeardata} charttype={'bar'} />} />
-            <Route path='/industries' element={<Industries data={industrydata} />} />
-            <Route path='/viewindustry' element={<View data={industrydata} charttype={'line'}/>} />
-          </Routes>
-        </Router>
-      </React.StrictMode>
-    )
-  })
+fetchdata().then(() => {
+  root.render(
+    <React.StrictMode>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/years" element={<Years data={yeardata} />} />
+          <Route
+            path="/viewyear"
+            element={<View data={yeardata} charttype={"bar"} />}
+          />
+          <Route
+            path="/industries"
+            element={<Industries data={industrydata} />}
+          />
+          <Route
+            path="/viewindustry"
+            element={<View data={industrydata} charttype={"line"} />}
+          />
+        </Routes>
+      </Router>
+    </React.StrictMode>
+  );
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
--- Frontend/src/years.js
+++ Frontend/src/years.js
@@ -1,28 +1,35 @@
 import { useNavigate } from "react-router-dom";
-import { useState, useEffect } from "react";
 import BottomNav from "./bottomNav";
 import CenterDiv from "./components/center_div";
 import Button from "./components/button";
 
 function Years({ data }) {
-    const navigate = useNavigate();
-    function navigateToViewyear(event) {
-        navigate('/viewyear', { state: { detail: event.target.textContent } });
-    }
+  const navigate = useNavigate();
+  function navigateToViewyear(event) {
+    navigate("/viewyear", { state: { detail: event.target.textContent } });
+  }
 
-    return (
-        <>
-            <CenterDiv>
-                <h2>View By Year</h2>
-                {data.length === 0 ? <h3>Loading...</h3> :
-                    data.map((year) => (
-                        <Button key={year.Sort_type} message={year.Sort_type} onClick={navigateToViewyear} />
-                    ))
-                }
-            </CenterDiv>
-            <BottomNav />
-        </>
-    )
+  return (
+    <>
+      <CenterDiv>
+        <h2>View By Year</h2>
+        {data.length === 0 ? (
+          <h3>Loading...</h3>
+        ) : (
+          data
+            .sort((a, b) => parseInt(a.Sort_type) - parseInt(b.Sort_type))
+            .map((year) => (
+              <Button
+                key={year.Sort_type}
+                message={year.Sort_type}
+                onClick={navigateToViewyear}
+              />
+            ))
+        )}
+      </CenterDiv>
+      <BottomNav />
+    </>
+  );
 }
 
 export default Years;
